5482f3812724a2cb211fbb876cf724e2ae06746b
VladSavin <vlad24077@gmail.com>
2021-03-05 17:53:11 +0300

    First commit

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+
+    void Start()
+    {
+        int vertexIndex = 0;
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        //Loops for creating full voxel
+        for (int p = 0; p < 6; p++)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                int triangleIndex = VoxelData.voxelTris[p, i];      //using for pulling numbers from voxelTris element
+                vertices.Add(VoxelData.voxelVerts[triangleIndex]);  //using for adding one of verts from voxelVerts
+                triangles.Add(vertexIndex);                         //every single triangle in right order
+
+                uvs.Add(VoxelData.voxelUvs [i]);                    //Position of texture
+
+                vertexIndex++;
+
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+    }
+}
